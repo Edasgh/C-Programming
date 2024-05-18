e94762f7c4201bd505f0802bad9a7bea046887f1
Edasgh <edas25564@gmail.com>
2024-05-18 20:45:06 +0530

    "Unneccesary files deleted"

--- Self/sprgm.c
+++ Self/sprgm.c
@@ -2,7 +2,7 @@
 #include <conio.h>
 #define MAX 100 // this is a constant
 
-int a = 50;
+int a = 50; // this is a normal variable
 void display();
 void main()
 {
@@ -34,6 +34,22 @@ void main()
         i++;
         printf("%d X %d = %d\n", num, i, num * i);
     } while (i < 10);
+
+//Manual approach
+//     int a;
+//    printf("Enter the number you want a multiplication table of\n");
+//    scanf("%d",&a);
+//    printf("Multiplication table of %d is as follows\n",a);
+//    printf("%d x 1 = %d\n",a,a*1);
+//    printf("%d x 2 = %d\n",a,a*2);
+//    printf("%d x 3 = %d\n",a,a*3);
+//    printf("%d x 4 = %d\n",a,a*4);
+//    printf("%d x 5 = %d\n",a,a*5);
+//    printf("%d x 6 = %d\n",a,a*6);
+//    printf("%d x 7 = %d\n",a,a*7);
+//    printf("%d x 8 = %d\n",a,a*8);
+//    printf("%d x 9 = %d\n",a,a*9);
+//    printf("%d x 10 = %d\n",a,a*10);
 }
 void display()
 {
